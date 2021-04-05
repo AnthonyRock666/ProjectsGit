ac77b03e2430465a5ae1f8cb1b0191569ba72d1d
Anthony Machado <anthonyklm386@gmail.com>
2021-04-05 13:28:41 -0300

    Mentalismo

---
+++ devmentalismo/README.markdown
@@ -0,0 +1,5 @@
+# DevMentalismo
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/TonyMachDev/pen/oNBXQyw](https://codepen.io/TonyMachDev/pen/oNBXQyw).
+
+
---
+++ devmentalismo/dist/script.js
@@ -0,0 +1,21 @@
+var numSecreto = parseInt (Math.random() * 20)
+ var tentativa = 4
+
+ while (tentativa > 0) {
+   var chute = parseInt(prompt('Insira um número de 0 a 20:')) 
+   
+   if (numSecreto == chute) {
+     alert ('Você acertou!!!')
+     break
+   } else if (chute > numSecreto) {
+     alert ('Tente novamente... Pois o número secreto é menor...')
+     tentativa = tentativa - 1
+   } else if (chute < numSecreto) {
+     alert ('Tente outra vez... O número secreto é maior...')
+     tentativa = tentativa - 1
+   }
+ }
+
+if (chute != numSecreto) {
+  alert ('Aaaa... Você não tem mais tentativas... Mas o número secreto era: ' + numSecreto)
+}
